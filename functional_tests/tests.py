80758c81c08e1fe460e113ec18f38d6c557b1073
patrickleonard <patrickleonard789@gmail.com>
2016-10-12 16:48:41 -0700

    Changed the functional tests to enable test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
@@ -5,6 +6,20 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+             if 'liveserver' in arg:
+                 cls.server_url = 'http://' + arg.split('=')[1]
+                 return
+        super(NewVisitorTest,cls).setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super(NewVisitorTest,cls).tearDownClass()
+            
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
     firefox_path='C:\\Program Files\\Mozilla Firefox-ESR\\firefox.exe'
@@ -16,7 +31,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         #Sally goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         #She she notices the input box is nicely centered
@@ -43,7 +58,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Sally heard about a cool cosplay page and wanted to check out its
         #homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #She notices the page title and header mentions SuperLists
         self.assertIn('To-Do lists' , self.browser.title)
@@ -85,7 +100,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         #Francis visits the home page. There is no sign of Sally's
         #list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Some Cosplay', page_text)
         self.assertNotIn('Another crappy cosplay', page_text)
