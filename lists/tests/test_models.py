27aae8d4772f3472703fbc30d173d210ade30c9a
Patrick Leonard <patrickleonard789@gmail.com>
2016-10-16 17:19:33 -0700

    Refactor list view to handle new item POSTs

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from lists.models import Item, List
 
 class ListAndItemModelsTest(TestCase):
-
     
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
