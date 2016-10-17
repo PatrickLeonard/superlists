30803bad76f067bbc56ec126712e642963206194
Patrick Leonard <patrickleonard789@gmail.com>
2016-10-16 17:34:27 -0700

    Use get_absolute_url on List model to remove duplicate urls in views

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,7 +3,11 @@ from django.test import TestCase
 from lists.models import Item, List
 
 class ListAndItemModelsTest(TestCase):
-    
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+        
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
